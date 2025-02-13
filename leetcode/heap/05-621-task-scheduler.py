b4abe39f3aa3c366e7f91d2a2286e3258ef99614
Raagul <beyondtheinfernotech@gmail.com>
2025-02-12 22:32:51 -0800

    more heaps

---
+++ leetcode/heap/05-621-task-scheduler.py
@@ -0,0 +1,87 @@
+'''
+Wrong, dunno where
+'''
+import heapq
+from collections import Counter, deque
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        task_count = Counter(tasks)
+        max_heap = []
+        for key, count in task_count.items():
+            heapq.heappush(max_heap, (-count, key))
+
+        time = 0
+        task_queue = deque([])
+        while max_heap or task_queue:
+            time += 1
+            # if time == 10:
+            #     break
+            curr_task = None
+            if task_queue and task_queue[0][2] == time:
+                curr_task = task_queue.popleft()
+            elif max_heap:
+                curr_task = heapq.heappop(max_heap)
+                # Removing the max_heap effect
+                curr_task = (-curr_task[0], curr_task[1])
+            else:
+                continue
+            # If count for task exists, do it and decrease count
+            if curr_task and curr_task[0] - 1 > 0:
+                next_available_at = time + n + 1
+                task_queue.append(
+                    (curr_task[0] - 1, curr_task[1], next_available_at)
+                )
+            # print(time)
+            # print(curr_task)
+            # print(max_heap)
+            # print(task_queue)
+            # print()
+        return time
+
+
+'''
+Greedy approach
+Time: O(T log K)
+  T - total number of tasks, K = number of unique tasks
+    - counter - O(T)
+    - max heap - O(K log K)
+    - while loop - O(T log K)
+    - cooldown - O(T)
+'''
+from collections import deque
+import heapq
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        '''
+        - greedy approach is needed - start with tasks that occur more 
+        no of times, cauz scheduling them later will lead to a lot of extra time
+        - max heap to process most frequent, and queue to track cooldown
+        '''
+        counter = {}
+        for task in tasks:
+            counter[task] = counter.get(task, 0) + 1
+        
+        # max heap - process tasks with more count first so that
+        # there is no idle time later on
+        heap = []
+        for k, v in counter.items():
+            heapq.heappush(heap, (-v, k))
+        
+        cooldown = deque()
+        time = 0
+        while heap or cooldown:
+            if heap:
+                count, taskId = heapq.heappop(heap)
+                # negate the value (from max heap)
+                count = -count
+                if count > 1:
+                    cooldown.append((count-1, taskId, time+n+1))
+            time += 1
+
+            while cooldown and cooldown[0][2] == time:
+                count, taskId, exitTime = cooldown.popleft()
+                heapq.heappush(heap, (-count, taskId))
+
+        return time
---
+++ leetcode/heap/06-2365-task-scheduler-ii.py
@@ -0,0 +1,49 @@
+'''
+Right answer, but Time limit Exceeded.
+Solution with heap and queue
+Time complexity: O(T) - where T is the time taken to process all the tasks
+'''
+from collections import deque
+
+class Solution:
+    def taskSchedulerII(self, tasks: List[int], space: int) -> int:
+        queue = deque()
+        cooldown = set()
+        time = 0
+        i = 0
+        while i < len(tasks):
+            # Check if tasks can be removed first
+            # cauz - you need to process at the given time if you are freeing it
+            while queue and queue[0][1] <= time:
+                task, _ = queue.popleft()
+                cooldown.remove(task)
+
+            if tasks[i] not in cooldown:
+                queue.append((tasks[i], time + space + 1))
+                cooldown.add(tasks[i])
+                i += 1
+                
+            time += 1
+
+        return time
+
+
+'''
+Simpler solution
+Time complexity: O(n)
+Space complexity: O(n)
+'''
+from collections import deque
+
+class Solution:
+    def taskSchedulerII(self, tasks: List[int], space: int) -> int:
+        lastSeen = {}
+        time = 0
+
+        for task in tasks:
+            if task in lastSeen:
+                time = max(time, lastSeen[task] + space + 1)
+            lastSeen[task] = time
+            time += 1
+
+        return time
